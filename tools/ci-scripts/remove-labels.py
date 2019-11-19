5367a83d97a6f22807d5f0c595696fdc845a94a1
Brad Davis <bdavis@saintandreas.org>
2019-11-19 14:03:08 -0800

    Only run unlabel for specific labels

--- tools/ci-scripts/remove-labels.py
+++ tools/ci-scripts/remove-labels.py
@@ -1,9 +1,11 @@
-import os, json
+import os, json, base64
 from github import Github
 
 label_prefix = os.environ['LABEL_PREFIX']
+github_token = os.environ['GITHUB_TOKEN']
 context = json.loads(os.environ['GITHUB_CONTEXT'])
-pr = Github(os.environ['GITHUB_TOKEN']).get_repo(context['repository']).get_pull(context['event']['number'])
+
+pr = Github(context['token']).get_repo(context['repository']).get_pull(context['event']['number'])
 for label in pr.get_labels():
     if label.name.startswith(label_prefix):
         pr.remove_from_labels(label.name)
